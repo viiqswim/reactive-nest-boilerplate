2ff2d74a07db22e14ab972e46522a8e4bcbd1793
Victor Dozal <v.dozal@live.com>
2020-09-05 18:30:29 -0500

    👌 IMPROVE: Enabled CORS

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-
+  app.enableCors();
   await app.listen(3001);
 }
 bootstrap();

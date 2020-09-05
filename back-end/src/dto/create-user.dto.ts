5b1e4a9b3cc56c50651f8ced84423c3214b8f127
Victor Dozal <v.dozal@live.com>
2020-09-05 16:06:47 -0500

    📦 NEW: Added sample API DTO field descriptions which are visible in the Swagger UI

--- back-end/src/dto/create-user.dto.ts
+++ back-end/src/dto/create-user.dto.ts
@@ -1,4 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'The first name of the user',
+  })
   firstName: string;
+
+  @ApiProperty({
+    description: 'The last name of the user',
+  })
   lastName: string;
 }

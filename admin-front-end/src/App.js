fdffb9b6ce0467576cda6c22f816e718ab388a84
Victor Dozal <v.dozal@live.com>
2020-09-07 00:15:10 -0500

    👌 IMPROVE: Added missing new line

--- admin-front-end/src/App.js
+++ admin-front-end/src/App.js
@@ -4,6 +4,7 @@ import { Admin, Resource, ListGuesser } from 'react-admin';
 import crudProvider from 'ra-data-nestjsx-crud'
 
 const dataProvider = crudProvider('http://localhost:3001');
+
 const App = () => (
     <Admin dataProvider={dataProvider}>
         <Resource name="companies" list={ListGuesser} />

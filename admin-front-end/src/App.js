3d8a0eb28c9fab4543f745191af5b09440cf539a
Victor Dozal <v.dozal@live.com>
2020-09-07 00:18:35 -0500

    📦 NEW: Added a way to show an individual Companies record as well as a way to edit the record through the admin interface

--- admin-front-end/src/App.js
+++ admin-front-end/src/App.js
@@ -1,13 +1,19 @@
 // in src/App.js
 import * as React from "react";
-import { Admin, Resource, ListGuesser } from 'react-admin';
+import {
+  Admin,
+  Resource,
+  ListGuesser,
+  ShowGuesser,
+  EditGuesser,
+} from 'react-admin';
 import crudProvider from 'ra-data-nestjsx-crud'
 
 const dataProvider = crudProvider('http://localhost:3001');
 
 const App = () => (
     <Admin dataProvider={dataProvider}>
-        <Resource name="companies" list={ListGuesser} />
+        <Resource name="companies" list={ListGuesser} show={ShowGuesser} edit={EditGuesser} />
     </Admin>
 );
 export default App;

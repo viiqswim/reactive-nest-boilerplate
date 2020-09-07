db7a6c60bc4b6cf0eb705f7adf442b541bce7d77
Victor Dozal <v.dozal@live.com>
2020-09-07 01:41:15 -0500

    📦 NEW: Added the morgan and compression packages and added them to the back-end bootstrap function

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -2,6 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
+import * as morgan from 'morgan';
+import * as compression from 'compression';
+
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -15,6 +19,9 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.enableCors();
+  app.use(compression());
+  app.use(morgan('combined'));
+
   await app.listen(3001);
 }
 bootstrap();

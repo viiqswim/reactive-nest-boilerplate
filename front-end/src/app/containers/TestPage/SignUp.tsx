80d8b5a9d37a4a4f31350cadc5b002ff931fdf89
Victor Dozal <v.dozal@live.com>
2020-09-08 23:22:04 -0500

    👌 IMPROVE: Removed useless import

--- front-end/src/app/containers/TestPage/SignUp.tsx
+++ front-end/src/app/containers/TestPage/SignUp.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Input, Button, Checkbox } from 'antd';
+import { Form, Input, Button } from 'antd';
 import { Wrapper } from 'app/components/Wrapper';
 
 export function SignUp() {

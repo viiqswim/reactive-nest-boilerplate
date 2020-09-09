9d16f11243c98bf26139c36c9e178d923bb38bbd
Victor Dozal <v.dozal@live.com>
2020-09-09 02:49:55 -0500

    👌 IMPROVE: Removed useless code

--- front-end/src/app/containers/LogInSignUp/saga.ts
+++ front-end/src/app/containers/LogInSignUp/saga.ts
@@ -1,40 +1,9 @@
 import { call, put, select, takeLatest, delay } from 'redux-saga/effects';
-import { request } from 'utils/request';
 import { selectUserEmail, selectPassword, selectIsLoggedIn } from './selectors';
 import { actions } from './slice';
 import { User } from 'types/User';
 import { UserErrorType } from './types';
 import { firebaseApp } from '../../../firebaseApp';
-
-/**
- * Github user request/response handler
- */
-export function* getUser() {
-  yield delay(500);
-  // Select userEmail from store
-  const userEmail: string = yield select(selectUserEmail);
-  if (userEmail.length === 0) {
-    yield put(actions.userError(UserErrorType.USER_ID_EMPTY));
-    return;
-  }
-  const requestURL = `${process.env.REACT_APP_BACK_END_URL}/users/${userEmail}`;
-
-  try {
-    // Call our request helper (see 'utils/request')
-    const user: User = yield call(request, requestURL);
-    if (user.id) {
-      yield put(actions.userLoaded(user));
-    } else {
-      yield put(actions.userError(UserErrorType.USER_HAS_NO_USER));
-    }
-  } catch (err) {
-    if (err.response?.status === 404) {
-      yield put(actions.userError(UserErrorType.USER_NOT_FOUND));
-    } else {
-      yield put(actions.userError(UserErrorType.RESPONSE_ERROR));
-    }
-  }
-}
 
 export function* loginUser() {
   yield delay(500);
@@ -91,7 +60,10 @@ export async function login(userEmail: string, password: string): Promise<any> {
     .auth()
     .signInWithEmailAndPassword(userEmail, password);
 
-  return response?.user;
+  return {
+    uid: response?.user?.uid,
+    userEmail: response?.user?.email,
+  };
 }
 
 export async function logout(): Promise<any> {
@@ -116,7 +88,6 @@ export function* logInSignUpSaga() {
   // By using `takeLatest` only the result of the latest API call is applied.
   // It returns task descriptor (just like fork) so we can continue execution
   // It will be cancelled automatically on component unmount
-  yield takeLatest(actions.loadUser.type, getUser);
   yield takeLatest(actions.loginUser.type, loginUser);
   yield takeLatest(actions.logoutUser.type, logoutUser);
 }

13d407c4d4ee4540f9c78eed46b8939271a536f6
Victor Dozal <v.dozal@live.com>
2020-09-08 23:08:15 -0500

    📦 NEW: Added sample navigation through react-router-dom

--- front-end/src/app/containers/NavBar/Nav.tsx
+++ front-end/src/app/containers/NavBar/Nav.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import styled from 'styled-components/macro';
+import { Link } from 'react-router-dom';
+
 import { ReactComponent as DocumentationIcon } from './assets/documentation-icon.svg';
 import { ReactComponent as GithubIcon } from './assets/github-icon.svg';
 
 export function Nav() {
   return (
     <Wrapper>
+      <ItemLink to="/">
+        <DocumentationIcon />
+        Home Page
+      </ItemLink>
+      <ItemLink to="/test">
+        <DocumentationIcon />
+        Test Page
+      </ItemLink>
       <Item
         href="https://cansahin.gitbook.io/react-boilerplate-cra-template/"
         target="_blank"
@@ -34,6 +44,29 @@ const Wrapper = styled.nav`
 `;
 
 const Item = styled.a`
+  color: ${p => p.theme.primary};
+  cursor: pointer;
+  text-decoration: none;
+  display: flex;
+  padding: 0.25rem 1rem;
+  font-size: 0.875rem;
+  font-weight: 500;
+  align-items: center;
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  &:active {
+    opacity: 0.4;
+  }
+
+  .icon {
+    margin-right: 0.25rem;
+  }
+`;
+
+const ItemLink = styled(Link)`
   color: ${p => p.theme.primary};
   cursor: pointer;
   text-decoration: none;

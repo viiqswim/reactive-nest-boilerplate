fce1360075ec32ad9a1ac0a6225cfc140d88e40d
Victor Dozal <v.dozal@live.com>
2020-09-09 02:20:37 -0500

    👌 IMPROVE: Created file to initialize firebase

---
+++ front-end/src/firebaseApp.ts
@@ -0,0 +1,8 @@
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
+export const firebaseApp = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+});

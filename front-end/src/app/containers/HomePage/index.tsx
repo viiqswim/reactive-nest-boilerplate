535789a2b28958e603358c574285baf99e08eeb8
Victor Dozal <v.dozal@live.com>
2020-09-09 21:49:57 -0500

    📦 NEW: Improved the application's layout by utilizing ant design

--- front-end/src/app/containers/HomePage/index.tsx
+++ front-end/src/app/containers/HomePage/index.tsx
@@ -1,25 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Layout, Menu, Button } from 'antd';
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from '@ant-design/icons';
 import { Helmet } from 'react-helmet-async';
-import { NavBar } from '../NavBar';
+// import { NavBar } from '../NavBar';
 import { Masthead } from './Masthead';
 import { Features } from './Features';
-import { PageWrapper } from 'app/components/PageWrapper';
+// import { PageWrapper } from 'app/components/PageWrapper';
+
+const { Header, Sider, Content } = Layout;
 
 export function HomePage() {
+  const [collapsed, setCollapsed] = useState(false);
+  const toggleCollapse = () => setCollapsed(!collapsed);
+  const collapsedMenuIcon = collapsed ? (
+    <MenuUnfoldOutlined />
+  ) : (
+    <MenuFoldOutlined />
+  );
+
   return (
     <>
       <Helmet>
         <title>Home Page</title>
         <meta
           name="description"
-          content="A React Boilerplate application homepage"
+          content="A Reactive Nest Boilerplate application homepage"
         />
       </Helmet>
-      <NavBar />
-      <PageWrapper>
-        <Masthead />
-        <Features />
-      </PageWrapper>
+      <Layout>
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              nav 1
+            </Menu.Item>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+              nav 2
+            </Menu.Item>
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+              Collapse Nav
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }}>
+            <Button
+              type="link"
+              icon={collapsedMenuIcon}
+              onClick={toggleCollapse}
+            />
+            {React.createElement(
+              collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+              {
+                className: 'trigger',
+                onClick: toggleCollapse,
+              },
+            )}
+          </Header>
+          <Content
+            className="site-layout-background"
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 280,
+            }}
+          >
+            <Masthead />
+            <Features />
+          </Content>
+        </Layout>
+      </Layout>
     </>
   );
 }

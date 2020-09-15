729417e909fb15cd61f0382a501934fa960f7e60
Victor Dozal <v.dozal@live.com>
2020-09-15 01:14:59 -0500

    👌 IMPROVE: Renamed from .env.prod to .env.production

--- back-end/src/app/config/dotEnv.config.ts
+++ back-end/src/app/config/dotEnv.config.ts
@@ -5,7 +5,7 @@ import { ConfigModule } from '@nestjs/config';
  */
 export const DotEnvConfig = ConfigModule.forRoot({
   // specify the .env files you want to use in the order of importance from left to right
-  envFilePath: ['./env/.env.prod', './env/.env.development'],
+  envFilePath: ['./env/.env.production', './env/.env.development'],
   // use 'ignoreEnvFile' if you wish to ignore the .env file and instead use hard-coded values
   ignoreEnvFile: false,
   // allow the app to use all of the .env values globally
